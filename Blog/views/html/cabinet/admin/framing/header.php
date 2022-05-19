6e94823937db026580deb0289abc6648343770f5
Arnas <worgenmagic@gmail.com>
2022-05-19 15:14:46 +0300

    project

--- Blog/views/html/cabinet/admin/framing/header.php
+++ Blog/views/html/cabinet/admin/framing/header.php
@@ -5,5 +5,6 @@
     <title>Admin Panel</title>
     <link rel="stylesheet" href="/views/template/css/normalize.css">
     <link rel="stylesheet" href="/views/template/css/admin.css">
+    <link rel="icon" href="/views/template/img/favicon.ico" type="image/x-icon">
 </head>
 <body>
